7b0ca9fe0315543ddb0cbfe86b7ae647c974e3d6
Miguel Martínez <migudevelop@gmail.com>
2022-09-25 19:35:18 +0200

    feat: add redux and eslint config with typescript

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+const RULES = {
+  OFF: 'off',
+  WARN: 'warn',
+  ERROR: 'error'
+}
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'plugin:prettier/recommended'
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json']
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': RULES.OFF,
+    'prettier/prettier': ['error', { endOfLine: 'auto' }]
+  }
+}
