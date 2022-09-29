afb8206a1b28dae7aba056ed58980a6d202db833
Miguel Martínez <migudevelop@gmail.com>
2022-09-29 15:54:37 +0200

    fix: fix typescript paths imports to jest

--- config/jest/jest.config.js
+++ config/jest/jest.config.js
@@ -8,5 +8,17 @@ module.exports = {
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
   modulePaths: ['<rootDir>/__tests__/'],
-  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx']
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  moduleNameMapper: {
+    '^@src/(.*)$': '<rootDir>/src/$1',
+    '^@app/(.*)$': '<rootDir>/src/app/$1',
+    '^@assets/(.*)$': '<rootDir>/src/assets/$1',
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@hooks/(.*)$': '<rootDir>/src/hooks/$1',
+    '^@locale/(.*)$': '<rootDir>/src/locale/$1',
+    '^@models/(.*)$': '<rootDir>/src/models/$1',
+    '^@redux/(.*)$': '<rootDir>/src/redux/$1',
+    '^@shared/(.*)$': '<rootDir>/src/shared/$1',
+    '^@styles/(.*)$': '<rootDir>/src/styles/$1'
+  }
 }
