16f43846e0ff0b2ee95f2a6ba48402d2e1989f28
Miguel Martínez <migudevelop@gmail.com>
2022-09-30 13:23:10 +0200

    feat: add feedback translates

--- src/locale/translations/index.ts
+++ src/locale/translations/index.ts
@@ -1,10 +1,12 @@
 import createPassword from './CreatePassword'
+import feedback from './Feedback'
 import productInformation from './ProductInformation'
 import shared from './shared'
 
 export default {
   es: {
     ...createPassword.es,
+    ...feedback.es,
     ...productInformation.es,
     ...shared.es
   }
